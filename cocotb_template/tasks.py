f53d3ac5044be2ab1580498da1bdb0bcdfeead98
Gary Lee <garywlee@gmail.com>
2025-11-18 08:06:48 +0800

    Support other waveform tools.

--- cocotb_template/tasks.py
+++ cocotb_template/tasks.py
@@ -16,10 +16,21 @@ def run(c):
     c.run("rm -f results.xml")
     c.run("make -f Makefile results.xml")
 
-@task
-def waveform(c):
+@task(help={"args": "Additional arguments for waveform viewer"})
+def waveform(c, args=""):
     """Show waveform."""
-    c.run("gtkwave sim_build/dut.fst")
+    tools = {
+        'surfer': '{fst_file}',
+        'gtkwave': '{fst_file}',
+    }
+    found_tools = [k for k in tools.keys() if which(k)]
+    if not found_tools:
+        print("No waveform viewer found (tried: " + ", ".join(tools.keys()) + ")")
+        return
+    cmd = found_tools[0]
+    tool_args = tools[cmd].format(fst_file='sim_build/dut.fst')
+    cmd_line = ' '.join([cmd, tool_args, args])
+    c.run(cmd_line)
 
 if __name__ == "__main__":
     namespace = Collection.from_module(sys.modules[__name__])
