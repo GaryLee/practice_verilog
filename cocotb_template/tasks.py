855ec22eb92c9a66edb546eccc759447b7010e96
GaryLee <garywlee@gmail.com>
2026-01-27 18:45:57 +0800

    Added missing `which` function.

--- cocotb_template/tasks.py
+++ cocotb_template/tasks.py
@@ -4,6 +4,24 @@
 import sys
 from invoke import task, Program, Collection
 
+def which(program):
+    """Check if a program exists in PATH."""
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
+
 @task
 def clean(c):
     """Clean the project."""
