6f896603f2a6fbe713d4c1be52dd2b81fb1854d7
Gary Lee <garywlee@gmail.com>
2026-02-14 00:21:30 +0800

    Added testcase for op_mult.

--- src/op_mult/dut.sv
+++ src/op_mult/dut.sv
@@ -5,21 +5,59 @@
 `timescale 1ns/100ps
 
 // This module is a simple adder that adds two 8-bit inputs and outputs the result.
-module dut (
-    input logic clk, 
-    input logic rst_n,
-    input logic [7:0] a, // TODO: Replace with your actual inputs
-    input logic [7:0] b, // TODO: Replace with your actual inputs
-    output logic [7:0] c // TODO: Replace with your actual inputs
+module dut #(parameter N = 8) (
+    input  logic clk, 
+    input  logic rst_n,
+    input  logic [N-1:0] a,
+    input  logic [N-1:0] b,
+    output logic [2*N-1:0] c_signed,
+    output logic           ov_signed,
+    output logic [2*N-1:0] c_unsigned,
+    output logic           ov_unsigned,
+    output logic [2*N-1:0] c_signed_sat,
+    output logic           ov_signed_sat,
+    output logic [2*N-1:0] c_unsigned_sat,
+    output logic           ov_unsigned_sat
 );
+    logic [N-1:0] op_a;
+    logic [N-1:0] op_b;
 
-// TODO: Replace following logic with your DUT implementation.
-always_ff @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        c <= 8'h00;
-    end else begin
-        c <= a + b;
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            op_a <= 0;
+            op_b <= 0;
+        end else begin
+            op_a <= a;
+            op_b <= b;
+        end
     end
-end 
+
+    op_mult #(.N(N), .SIGNED(0), .SATURATE(0)) u_op_mult_unsigned (
+        .a     (op_a),
+        .b     (op_b),
+        .result(c_unsigned),
+        .ov    (ov_unsigned)
+    );
+
+    op_mult #(.N(N), .SIGNED(0), .SATURATE(1)) u_op_mult_unsigned_sat (
+        .a     (op_a),
+        .b     (op_b),
+        .result(c_unsigned_sat),
+        .ov    (ov_unsigned_sat)
+    );
+
+    op_mult #(.N(N), .SIGNED(1), .SATURATE(0)) u_op_mult_signed (
+        .a     (op_a),
+        .b     (op_b),
+        .result(c_signed),
+        .ov    (ov_signed)
+    );
+
+    op_mult #(.N(N), .SIGNED(1), .SATURATE(1)) u_op_mult_signed_sat (
+        .a     (op_a),
+        .b     (op_b),
+        .result(c_signed_sat),
+        .ov    (ov_signed_sat)
+    );
 
 endmodule
--- src/op_mult/test_proc.py
+++ src/op_mult/test_proc.py
@@ -13,6 +13,26 @@ from cocotb import utils
 from sim_utils import *
 from itertools import permutations
 
+to_signed = lambda x: x if x < 128 else x - 256
+
+def mult_unsigned(a, b, sat=False):
+    a, b = a & 0xFF, b & 0xFF
+    c = a * b
+    ov = 1 if c > 0xFFFF else 0
+    c = 0xFFFF & c
+    if ov and sat:
+        c = 32767
+    return c, ov
+
+def mult_signed(a, b, sat=False):
+    a_signed, b_signed = to_signed(a), to_signed(b)
+    c = a_signed * b_signed
+    ov = 1 if c > 32767 or c < -32768 else 0
+    if ov and sat:
+        c = 32767 if c > 0 else -32768
+    c = 0xFFFF & c
+    return c, ov
+
 @cocotb.test()
 async def test_proc (dut):
     # Generate clocks and initialization.
@@ -28,12 +48,32 @@ async def test_proc (dut):
     await (dut.clk@posedge)
     dut.rst_n.value = 1
     await (dut.clk@posedge)
+
     for i, j in permutations(range(256), 2):
         dut.a.value = i
         dut.b.value = j
         await (2@cycles(dut.clk, rising=True))
-        result = dut.c.value
-        answer = (i + j) & 0xFF
-        assert result == answer, f"Result mismatch: {dut.a.value=}, {dut.b.value=}, {dut.c.value=}"
+        c_signed        = dut.c_signed.value.to_unsigned()
+        ov_signed       = int(dut.ov_signed.value)
+        c_unsigned      = dut.c_unsigned.value.to_unsigned()
+        ov_unsigned     = int(dut.ov_unsigned.value)
+        c_signed_sat    = dut.c_signed_sat.value.to_unsigned()
+        ov_signed_sat   = int(dut.ov_signed_sat.value)
+        c_unsigned_sat  = dut.c_unsigned_sat.value.to_unsigned()
+        ov_unsigned_sat = int(dut.ov_unsigned_sat.value)
+
+        answer_unsigned, answer_ov_unsigned = mult_unsigned(i, j, sat=False)
+        answer_unsigned_sat, answer_ov_unsigned_sat = mult_unsigned(i, j, sat=True)
+        answer_signed, answer_ov_signed = mult_signed(i, j, sat=False)
+        answer_signed_sat, answer_ov_signed_sat = mult_signed(i, j, sat=True)
+
+        assert c_signed == answer_signed                , f"Signed multiplication failed for {i} * {j}: got {c_signed}, expected {answer_signed}"
+        assert ov_signed == answer_ov_signed            , f"Signed multiplication overflow flag failed for {i} * {j}: got {ov_signed}, expected {answer_ov_signed}"
+        assert c_unsigned == answer_unsigned            , f"Unsigned multiplication failed for {i} * {j}: got {c_unsigned}, expected {answer_unsigned}"
+        assert ov_unsigned == answer_ov_unsigned        , f"Unsigned multiplication overflow flag failed for {i} * {j}: got {ov_unsigned}, expected {answer_ov_unsigned}"
+        assert c_signed_sat == answer_signed_sat        , f"Signed multiplication with saturation failed for {i} * {j}: got {c_signed_sat}, expected {answer_signed_sat}"
+        assert ov_signed_sat == answer_ov_signed_sat    , f"Signed multiplication with saturation overflow flag failed for {i} * {j}: got {ov_signed_sat}, expected {answer_ov_signed_sat}"
+        assert c_unsigned_sat == answer_unsigned_sat    , f"Unsigned multiplication with saturation failed for {i} * {j}: got {c_unsigned_sat}, expected {answer_unsigned_sat}"
+        assert ov_unsigned_sat == answer_ov_unsigned_sat, f"Unsigned multiplication with saturation overflow flag failed for {i} * {j}: got {ov_unsigned_sat}, expected {answer_ov_unsigned_sat}"
         
     dut._log.info("TEST DONE!")
